aaa5761bcbd5c2ad0e614fce77c9635289cb19d9
Jérémy Goupil <goupiljeremy@gmail.com>
2019-01-13 23:31:08 +0100

    Develop (#1)
    * script travis ci for testing
    
    * same
    
    * deploy on rinkeby blockchain
    
    * add gitignore node_modules

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
+const provider = new HDWalletProvider(
+  'exhibit sheriff mixture pear lady senior roast top flee mosquito glory card',
+  'https://rinkeby.infura.io/v3/f8a076d5128e42bd888bfe7ce0e94f1d'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
