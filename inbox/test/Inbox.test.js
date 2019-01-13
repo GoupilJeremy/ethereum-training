6a71a9bd0c5f8ac59363b23b95ee660f23bcd9ba
Jérémy Goupil <goupiljeremy@gmail.com>
2019-01-13 23:24:47 +0100

    deploy on rinkeby blockchain

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -16,7 +16,10 @@ beforeEach(async () => {
 
   // Use one those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
     .send({ from: accounts[0], gas: '1000000' });
 
   inbox.setProvider(provider);
@@ -27,8 +30,15 @@ describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
+
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, "Hi there!");
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
