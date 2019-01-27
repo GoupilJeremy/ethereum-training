c84bce330a8c000a6e28da2b3e924cfe9e00554c
Jérémy Goupil <goupiljeremy@gmail.com>
2019-01-27 21:27:14 +0100

    end of the web lottery game

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     manager: '',
     players: [],
     balance: '',
-    value: ''
+    value: '',
+    message: ''
   };
 
   async componentDidMount() {
@@ -23,7 +24,29 @@ class App extends Component {
   onSubmit = async (event) => {
     event.preventDefault();
 
-    
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction sucess...' });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'You have been entered!' });
+
+  };
+
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' });
   };
 
   render() {
@@ -49,6 +72,15 @@ class App extends Component {
             </div>
             <button>Enter</button>
           </form>
+
+          <hr />
+
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={this.onClick}>Pick a winner!</button>
+
+          <hr />
+
+          <h1>{this.state.message}</h1>
         </div>
     );
   }
