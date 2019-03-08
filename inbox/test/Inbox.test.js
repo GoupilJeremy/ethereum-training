728b2fb34b40227d7a9a2401235d4b35d86f4746
Jérémy Goupil <goupiljeremy@gmail.com>
2019-03-08 19:59:50 +0100

    ok

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -16,10 +16,7 @@ beforeEach(async () => {
 
   // Use one those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ['Hi there!']
-    })
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
 
   inbox.setProvider(provider);
@@ -30,15 +27,8 @@ describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
-
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, "Hi there!");
-  });
-
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
-    const message = await inbox.methods.message().call();
-    assert.equal(message, 'bye');
   });
 });
