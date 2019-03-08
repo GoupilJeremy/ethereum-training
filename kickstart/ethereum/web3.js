01a84896155fff9ded45206756d7b580b3aca3f1
Jérémy Goupil <goupiljeremy@gmail.com>
2019-03-08 19:52:39 +0100

    fixed

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,5 +1,13 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/f8a076d5128e42bd888bfe7ce0e94f1d"
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
