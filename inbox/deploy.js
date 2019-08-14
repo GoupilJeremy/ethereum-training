694534b13d36338af20bd92f97878ca078c7d4e3
Jérémy Goupil <goupiljeremy@gmail.com>
2019-08-14 13:42:36 +0200

    try resolved all tests on all projects

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,25 @@
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'exhibit sheriff mixture pear lady senior roast top flee mosquito glory card',
+    "https://rinkeby.infura.io/v3/e2e467093ddd4a1fa8c16831a3a9cb3d"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log(interface);
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
