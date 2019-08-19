9827811a31fb14ba71cdf842df004a6b62b9e255
Jérémy Goupil <goupiljeremy@gmail.com>
2019-08-19 11:00:40 +0200

    Develop (#18)
    * update package
    
    * remove node-modules
    
    * testing hooks
    
    * test
    
    * add semantic-ui-react and css
    
    * add create button
    
    * pass the test mtf....
    
    * add header and some children in layout
    
    * add some style
    
    * update travis removed incoming project not finished yet
    
    * update all projects for tests on CI
    
    * update travis config
    
    * add routing and create a new campaign
    
    * try resolved all tests on all projects
    
    * try resolved all tests on all projects
    
    * try resolved all tests on all projects
    
    * add campaign show with dynamic routing
    
    * add routing to campaigns/new
    
    * add routing to campaigns/show

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
+import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 class CampaignSow extends Component {
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4],
+        };
+    }
     render() {
-        return <h3>Campaign Show</h3>;
+        return (
+            <Layout>
+                <h3>Campaign Show</h3>
+
+            </Layout>
+        );
     }
 }
 
