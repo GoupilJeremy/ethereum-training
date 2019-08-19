decf6d25f4b0aceb74f362d27eb81b4a9f59d8e7
Jérémy Goupil <goupiljeremy@gmail.com>
2019-08-19 13:49:40 +0200

    Develop (#19)
    * update package
    
    * remove node-modules
    
    * testing hooks
    
    * test
    
    * add semantic-ui-react and css
    
    * add create button
    
    * pass the test mtf....
    
    * add header and some children in layout
    
    * add some style
    
    * update travis removed incoming project not finished yet
    
    * update all projects for tests on CI
    
    * update travis config
    
    * add routing and create a new campaign
    
    * try resolved all tests on all projects
    
    * try resolved all tests on all projects
    
    * try resolved all tests on all projects
    
    * add campaign show with dynamic routing
    
    * add routing to campaigns/new
    
    * add routing to campaigns/show
    
    * add campaing details campaigns/show
    
    * add contribute form

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import { Card } from 'semantic-ui-react';
+import web3 from '../../ethereum/web3';
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import ContributeForm from "../../components/ContributeForm";
 
 class CampaignSow extends Component {
     static async getInitialProps(props) {
@@ -14,11 +17,58 @@ class CampaignSow extends Component {
             manager: summary[4],
         };
     }
+    renderCards() {
+        const {
+            minimumContribution,
+            balance,
+            requestsCount,
+            approversCount,
+            manager,
+        } = this.props;
+
+        const items = [
+            {
+                header: manager,
+                meta: 'Address of Manager',
+                description: 'The manager created this campaign and can create requests to withdraw money',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: minimumContribution,
+                meta: 'minimum Contribution (wei)',
+                description: 'You must contribute at least this much wei to become an approver',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: requestsCount,
+                meta: 'Number of Requests',
+                description: 'A request tries to withdraw money from the contract. Request must be approved by approvers',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: approversCount,
+                meta: 'Number of Approvers',
+                description: 'Number of people who already donated to this campaign',
+                style: {overflowWrap: 'break-word'}
+            },
+            {
+                header: web3.utils.fromWei(balance, 'ether'),
+                meta: 'Campaign Balance (ether)',
+                description: 'The balance is how much money this campaign has left to spend.',
+                style: {overflowWrap: 'break-word'}
+            },
+
+        ];
+
+        return <Card.Group items={items} />
+    }
+
     render() {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
-
+                {this.renderCards()}
+                <ContributeForm />
             </Layout>
         );
     }
