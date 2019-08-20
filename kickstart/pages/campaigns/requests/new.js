9635f2d234fdf59f0e1d85904912f68adea0198f
Jérémy Goupil <goupiljeremy@gmail.com>
2019-08-20 15:57:21 +0200

    add new component form to create a new request and routing

---
+++ kickstart/pages/campaigns/requests/new.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class RequestNew extends Component {
+    render() {
+        return (
+            <h3>Request New</h3>
+        );
+    }
+}
+
+export default RequestNew;
