b5506b2cde80cea3876901d789791d69b51978f0
Jérémy Goupil <goupiljeremy@gmail.com>
2019-08-22 11:19:38 +0200

    add Table of requests

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -3,6 +3,7 @@ import Layout from "../../../components/Layout";
 import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
     static async getInitialProps(props){
@@ -19,6 +20,17 @@ class RequestIndex extends Component {
         );
 
         return {address, requests, requestCount};
+    }
+
+    renderRow() {
+        return this.props.requests.map((request, index) => {
+           return <RequestRow
+               key={index}
+               id={index}
+               request={request}
+               address={this.props.address}
+           />
+        });
     }
 
     render() {
@@ -44,6 +56,9 @@ class RequestIndex extends Component {
                             <HeaderCell>Finalize</HeaderCell>
                         </Row>
                     </Header>
+                    <Body>
+                        {this.renderRow()}
+                    </Body>
                 </Table>
             </Layout>
         );
